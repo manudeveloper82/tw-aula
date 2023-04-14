1fddc93f3b4268a2a93b9dd39d1d6481fe22d2fa
Manuela Gomes <manudeveloper82@gmail.com>
2023-04-14 16:46:10 -0300

    twiter-clone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostComponent } from './components/post/post.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { AboutComponent } from './components/about/about.component';
+import { BiografiaComponent } from './components/biografia/biografia.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostComponent,
+    TweetComponent,
+    AboutComponent,
+    BiografiaComponent,
+    HomeComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
